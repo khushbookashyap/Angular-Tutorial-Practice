c710bbcc3867cfe0c2cd8aa4f7600a9ce689e872
khushbookashyap <47290963+khushbookashyap@users.noreply.github.com>
2019-03-10 18:43:55 +0530

    TDF- bindind data to userModel
    User defined model using the constructor . Then using property binding to bind the form elements to userModel.
    also implemented the banana in a box xoncept for two way binding in the name and email tags.

--- angular Forms/tdf/src/app/app.component.ts
+++ angular Forms/tdf/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  topics =['angular','react','java'];
+  topics =['angular','react','java']; 
+
+  public userModel = new User('Khushboo','khushboo@tcs.com',9999999,'angular','morning',true);
 }
---
+++ angular Forms/tdf/src/app/user.ts
@@ -0,0 +1,14 @@
+export class User {
+
+    constructor(
+     
+        public userName : string,
+        public email : string,
+        public phone : number,
+        public choice : string,
+        public timePreference : string,
+        public subscribe : boolean,
+        
+    )
+    {}
+}
